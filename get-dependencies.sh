bd39103c01485a19fc11da3a93ed3cf4a968d90b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-20 19:46:36 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,10 +4,10 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-make-aur-package
-
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
+
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
+make-aur-package
