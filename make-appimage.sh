5aad5f5373bdfa244bcb02df3aae0ff2a3b3ef06
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-23 22:55:36 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/tuxfootball.png
 export DESKTOP=/usr/share/applications/tuxfootball.desktop
-export DEPLOY_PIPEWIRE=1
+export DEPLOY_PULSE=1
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
