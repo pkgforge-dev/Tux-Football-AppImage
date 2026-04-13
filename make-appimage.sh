67f65383305572eed8b806b3182f7f97a754be56
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-13 10:32:02 +0200

    Replace self-updater.bg.hook with self-updater.hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q tuxfootball | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/tuxfootball.png
 export DESKTOP=/usr/share/applications/tuxfootball.desktop
